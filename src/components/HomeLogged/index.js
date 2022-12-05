4461a1d15cdb08738509eb372c1683fda6bafb3f
Janderson Oliveira <jandersonturiista14@gmail.com>
2022-12-04 23:20:40 -0300

    improvement menu home logged

--- src/components/HomeLogged/index.js
+++ src/components/HomeLogged/index.js
@@ -1,7 +1,20 @@
 import React, { useState } from 'react';
-import { Container, ContainerSvg } from './HomeLoggedElements';
-import { FaBars } from 'react-icons/fa';
+// import { Container, ContainerSvg } from './HomeLoggedElements';
+import { FaBars, FaPeopleArrows, FaPlus, FaUserAlt } from 'react-icons/fa';
 import Sidebar from './Sidebar/index';
+import {
+  Form
+} from './HomeLoggedElements';
+import {
+  ServicesContainer,
+  ServicesCard,
+  ServicesH1,
+  ServicesH2,
+  ServicesIcon,
+  ServicesP,
+  ServicesWrapper
+} from '../Services/ServicesElements';
+import Scaffold from '../Scaffold';
 
 const HomeLogged = () => {
   const [sidebar, setSidebar] = useState(true);
@@ -9,7 +22,36 @@ const HomeLogged = () => {
   const showSiderbar = () => setSidebar(!sidebar);
   return (
     <>
-      <Container>
+      <Scaffold>
+        <Form>
+
+        <ServicesWrapper
+          smooth={true}
+          duration={500}
+          spy={true}
+          exact='true'
+          offset={-80}
+        >
+        <ServicesCard>
+            {/* <ServicesIconIcon src={Icon1} /> */}
+            <FaPlus />
+            <ServicesP>Create Key</ServicesP>
+        </ServicesCard>
+        <ServicesCard>
+        <FaPeopleArrows />
+            {/* <ServicesIconIcon src={Icon1} /> */}
+            <ServicesP>Transfer</ServicesP>
+        </ServicesCard>
+        <ServicesCard>
+          <FaUserAlt />
+            {/* <ServicesIconIcon src={Icon1} /> */}
+            <ServicesP>My Keys</ServicesP>
+        </ServicesCard>
+        </ServicesWrapper>
+        </Form>
+
+      </Scaffold>
+      {/* <Container>
         <ContainerSvg>
           <FaBars onClick={showSiderbar} />
           {sidebar && <Sidebar active={setSidebar} />}
@@ -17,7 +59,7 @@ const HomeLogged = () => {
         <div>
           <h5 style={{ fontSize: 'large' }}>BlessPay</h5>
         </div>
-      </Container>
+      </Container> */}
     </>
   );
 };
